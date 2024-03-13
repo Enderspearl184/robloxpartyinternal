f373149b5e6f0d5a6e7c7c791d18976a7573266a
Enderspearl184 <enderspearl184@gmail.com>
2024-03-13 16:21:38 -0700

    Slight Oopsie Daisy, don't spam log that

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const http = require('http')
 const cookies = JSON.parse(process.env.cookies)
 
 setInterval(async function() {
+    console.debug("Refreshing online users..")
     for (let cookie of cookies) {
-        console.debug("Refreshing online users..")
         fetch("https://www.roblox.com/home",{headers:{cookie:".ROBLOXSECURITY="+cookie}})
     }
 },60000)
