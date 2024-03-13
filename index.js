1a97283409b5310241d2d15b43d1a9e591418329
Enderspearl184 <enderspearl184@gmail.com>
2024-03-13 16:39:27 -0700

    output response of presence request

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ async function refreshUsers() {
     console.debug("Refreshing online users..")
     for (let cookie of cookies) {
         //use studio url as that counts as Online i think
-        fetch("https://assetgame.roblox.com/Game/ClientPresence.ashx?version=old&PlaceID=10709056285&LocationType=Studio",{headers:{"User-Agent":"RobloxStudio/WinInet RobloxApp/0.616.0.6160659 (GlobalDist; RobloxDirectDownload)",Cookie:".ROBLOXSECURITY="+cookie},credentials:"include"})
+        fetch("https://assetgame.roblox.com/Game/ClientPresence.ashx?version=old&PlaceID=10709056285&LocationType=Studio",
+        {headers:{"User-Agent":"RobloxStudio/WinInet RobloxApp/0.616.0.6160659 (GlobalDist; RobloxDirectDownload)",Cookie:".ROBLOXSECURITY="+cookie},credentials:"include"}
+        ).then(res=>res.text().then(console.debug))
     }
 }
 setInterval(refreshUsers,60000)
