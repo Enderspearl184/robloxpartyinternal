1f5292239d346441ae0f6b9e4d770085ed8b1a6b
Enderspearl184 <enderspearl184@gmail.com>
2024-03-13 16:15:08 -0700

    Ensure accounts remain Online

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const fetch = require("node-fetch")
 const http = require('http')
 const cookies = JSON.parse(process.env.cookies)
-console.log(cookies)
+
+setInterval(async function() {
+    for (let cookie of cookies) {
+        fetch("https://www.roblox.com/home",{headers:{cookie:".ROBLOXSECURITY="+cookie}})
+    }
+},60000)
