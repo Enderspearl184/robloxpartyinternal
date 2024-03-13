51d1af61d7aee2b4a044bc45324e82ce10a6c42b
Enderspearl184 <enderspearl184@gmail.com>
2024-03-13 16:47:22 -0700

    Apparently I typoed .ROBLOSECURITY

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ async function refreshUsers() {
     for (let cookie of cookies) {
         //use studio url as that counts as Online i think
         fetch("https://assetgame.roblox.com/Game/ClientPresence.ashx?version=old&PlaceID=10709056285&LocationType=Studio",
-        {headers:{"User-Agent":"RobloxStudio/WinInet RobloxApp/0.616.0.6160659 (GlobalDist; RobloxDirectDownload)",Cookie:".ROBLOXSECURITY="+cookie},credentials:"include"}
+        {headers:{"User-Agent":"RobloxStudio/WinInet RobloxApp/0.616.0.6160659 (GlobalDist; RobloxDirectDownload)",Cookie:".ROBLOSECURITY="+cookie},credentials:"include"}
         )
 
         fetch("https://users.roblox.com/v1/users/authenticated",
-        {headers:{"User-Agent":"RobloxStudio/WinInet RobloxApp/0.616.0.6160659 (GlobalDist; RobloxDirectDownload)",Cookie:".ROBLOXSECURITY="+cookie},credentials:"include"}
+        {headers:{"User-Agent":"RobloxStudio/WinInet RobloxApp/0.616.0.6160659 (GlobalDist; RobloxDirectDownload)",Cookie:".ROBLOSECURITY="+cookie},credentials:"include"}
         ).then(res=>res.text().then(console.debug))
     }
 }
