faf9bdd4069c952b208304f74870b6de1d8666f4
Enderspearl184 <enderspearl184@gmail.com>
2024-03-13 16:32:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookies = JSON.parse(process.env.cookies)
 setInterval(async function() {
     console.debug("Refreshing online users..")
     for (let cookie of cookies) {
-        fetch("https://www.roblox.com/home",{headers:{Cookie:".ROBLOXSECURITY="+cookie},credentials:"include"})
+        //use studio url as that counts as Online i think
+        fetch("https://assetgame.roblox.com/Game/ClientPresence.ashx?version=old&PlaceID=10709056285&LocationType=Studio",{headers:{Cookie:".ROBLOXSECURITY="+cookie},credentials:"include"})
     }
 },60000)
