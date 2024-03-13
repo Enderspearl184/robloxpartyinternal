2db723a1a4debbaa2dba4d6ce80b33645ee972e4
Enderspearl184 <enderspearl184@gmail.com>
2024-03-13 16:33:48 -0700

    Refresh users on startup

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const fetch = require("node-fetch")
 const http = require('http')
 const cookies = JSON.parse(process.env.cookies)
 
-setInterval(async function() {
+async function refreshUsers() {
     console.debug("Refreshing online users..")
     for (let cookie of cookies) {
         //use studio url as that counts as Online i think
         fetch("https://assetgame.roblox.com/Game/ClientPresence.ashx?version=old&PlaceID=10709056285&LocationType=Studio",{headers:{Cookie:".ROBLOXSECURITY="+cookie},credentials:"include"})
     }
-},60000)
+}
+setInterval(refreshUsers,60000)
+refreshUsers()
